1c9d27b655916faf6a3eedfb894fbc50ffc2319f
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-05 08:34:37 +0100

    self.items now stored in 2d array

--- code/inventory.py
+++ code/inventory.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import json
+import math
 
+from pprint import pprint
 from entities import Rect
 from config import (
     SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, INVENTORY_WIDTH, INVENTORY_HEIGHT,
@@ -16,6 +18,7 @@ class InventoryItem:
         self.name = name
         self.qty = qty
 
+
     def draw(self, xpos, ypos):
         pg.draw.rect(SCREEN, self.colour, pg.Rect(
             xpos, ypos, self.width, self.height
@@ -28,10 +31,19 @@ class Inventory(Rect):
         super().__init__(colour, xpos, ypos, width, height)
         self._save_file = 'inventory.json'
         self.open = False
+        self.rows = INVENTORY_HEIGHT // INVENTORY_BOX_SIZE
+        self.cols = INVENTORY_WIDTH // INVENTORY_BOX_SIZE
 
     def load_inventory(self):
         with open(self._save_file, 'r') as inventory:
             self.items = json.loads(inventory.read())['items']
+            self.items = [InventoryItem(**item) for item in self.items]
+            
+            used_rows = math.ceil(len(self.items) / self.cols)
+            self.items = [
+                self.items[(i-1) * self.cols : i * self.cols] 
+                for i in range(1, used_rows+1)
+            ]
 
     def shade_boxes(self):
         for i in range(INVENTORY_HEIGHT // INVENTORY_BOX_SIZE):
@@ -44,20 +56,19 @@ class Inventory(Rect):
                 ))
 
     def display(self):
-        rows = INVENTORY_WIDTH // INVENTORY_BOX_SIZE
-
-        for i, item in enumerate(self.items):
-            item = InventoryItem(**item)
-            item.draw(
-                ((i % rows) * INVENTORY_BOX_SIZE + self.xpos) + ((INVENTORY_BOX_SIZE // 2) - (item.width//2)),
-                ((i // rows) * INVENTORY_BOX_SIZE) + self.ypos + ((INVENTORY_BOX_SIZE // 2) - (item.height//2))
-            )
+    
+        for i in range(len(self.items)):
+            for j in range(len(self.items[i])):
+                self.items[i][j].draw(
+                    (j * INVENTORY_BOX_SIZE) + self.xpos + (INVENTORY_BOX_SIZE // 2) - (self.items[i][j].width // 2),
+                    (i * INVENTORY_BOX_SIZE) + self.ypos + (INVENTORY_BOX_SIZE // 2) - (self.items[i][j].height // 2)
+                )
 
     def select_item(self, mouse_pos:tuple[int]):
         mouse_x = mouse_pos[0]
         mouse_y = mouse_pos[1]
 
-        box = (mouse_x // INVENTORY_BOX_SIZE, mouse_y // INVENTORY_BOX_SIZE)
+        box_x = (mouse_x // INVENTORY_BOX_SIZE) - ((SCREEN_WIDTH - INVENTORY_WIDTH) // 2) // INVENTORY_BOX_SIZE
+        box_y = ((2 * mouse_y) - SCREEN_HEIGHT + INVENTORY_HEIGHT) // (2 * INVENTORY_BOX_SIZE)
 
-        print(box)
-
+        print(box_x, box_y)
